b13b178e3c0bd8e6a73ba529948a3c37527faf17
Orynbek <orikentos2006@gmail.com>
2024-11-28 01:41:14 +0500

    add processed the functionality of the past date

--- src/js/main.js
+++ src/js/main.js
@@ -39,14 +39,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // таймер обратного отсчета
 
-    const deadline = '2024-11-30'; // строка с датой и временем оканчания
+    const deadline = '2022-11-30'; // строка с датой и временем оканчания
     
     function getTimeRemaining(endtime) { // эта функция вычисляет оставшееся время до указанной даты
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              hours = Math.floor(t / (1000 * 60 * 60) % 24),
-              minutes = Math.floor((t / 1000 / 60) % 60),
-              seconds = Math.floor((t / 1000) % 60);
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor(t / (1000 * 60 * 60) % 24),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        }
 
               return {
                 'total': t,
