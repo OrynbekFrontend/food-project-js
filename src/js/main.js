53a397dc63d69ecad2814eb8dbed88257bcad3a6
Orynbek <orikentos2006@gmail.com>
2024-11-28 01:31:01 +0500

    add reverse report timer

--- src/js/main.js
+++ src/js/main.js
@@ -36,4 +36,60 @@ window.addEventListener('DOMContentLoaded', () => {
             })
         }
     });
+
+    // таймер обратного отсчета
+
+    const deadline = '2024-11-30'; // строка с датой и временем оканчания
+    
+    function getTimeRemaining(endtime) { // эта функция вычисляет оставшееся время до указанной даты
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor(t / (1000 * 60 * 60) % 24),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+              return {
+                'total': t,
+                'days': days,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+              }
+    };
+
+    function getZero(num) { // добавляет 0 если ичисло меньше 10
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    };
+
+    function setClock(selector, endtime) { // основная функция. Находит элемент таймера по селектору.
+        // Обновляет отображение времени каждую секунду.
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock(); // чтобы при обновлений страницы таймер не обновлялся
+
+        function updateClock() { // выводит вычисления на экран и останавливает функ по истечения срока
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        };
+
+    };
+
+    setClock('.timer', deadline);
 });
