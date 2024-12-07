27ee4d304e6bb18d98f88a8269c8f2fc205f4157
Orynbek <orikentos2006@gmail.com>
2024-12-08 02:27:41 +0500

    add rest operator

--- src/js/main.js
+++ src/js/main.js
@@ -155,11 +155,12 @@ window.addEventListener('DOMContentLoaded', () => {
     // use class for card
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
+            this.classes = classes;
             this.price = price;
             this.transfer = 27;
             this.parentSelector = document.querySelector(parentSelector)
@@ -172,8 +173,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-                <div class="menu__item">
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.descr}</div>
@@ -182,7 +190,6 @@ window.addEventListener('DOMContentLoaded', () => {
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
-                </div>
                 `;
             this.parentSelector.append(element);
         }
